4814fbcd6a489c8aafd03c0faa540b74a4875d85
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2024-02-23 16:17:12 -0600

    Updated files.

--- code/R/results_summary.R
+++ code/R/results_summary.R
@@ -42,7 +42,16 @@ for (proj in c("I", "OV", "I-PC", "OV-PC","EFA")) {print(proj)
 }
 
 res$proj <- factor(res$proj, levels = c("EFA","I","OV","I-PC","OV-PC"))
-res <- res %>% filter(method != "XG")
+
+all <- do.call(rbind,results_rf_species_given_tribe[["I-PC"]])
+mean(all$pred_class == all$real_class)
+
+
+# EFA: 0.6268741
+# I: 0.7765205
+# OV:0.7711457
+# I-PC: 0.8169731
+# OV-PC: 0.8851485
 
 png("./figures/species_given_tribe_accuracy_size.png", res = 300, units = "in", h = 5, w = 8)
 library(ggplot2)
@@ -164,8 +173,11 @@ for (proj in c("I", "OV", "I-PC", "OV-PC","EFA")) {print(proj)
 
 
 res$proj <- factor(res$proj, levels = c("EFA","I","OV","I-PC","OV-PC"))
-res <- res %>% filter(method != "XG")
 res$model <- factor(res$model)
+
+#Overall classification rate
+all <- do.call(rbind,results_svm_radial_tribe[["I"]])
+mean(all$pred_class == all$real_class)
 
 
 png("./figures/tribe_accuracy_size.png", res = 300, units = "in", h = 5, w = 8)
@@ -174,9 +186,20 @@ ggplot(aes(x = proj, y = accuracy, colour = method, group = method), data = res)
   geom_line(aes(group = method))+ xlab("Feature Generation Method") + ylab("Accuracy")
 dev.off()
 
+#By tribe classification accuracy
+library(tidyverse)
+out <- lapply(results_svm_radial_tribe[["I"]], function(x){x %>% group_by(real_class) %>% summarise(mn = mean(pred_class == real_class), n = n())})
+
+acc_ind <- do.call(rbind,out)
+acc_ind$toothtype <- rep(names(out), each = 7)
+
+png("./full_shape_classification_SRVF/figures/tribe_accuracy_by_tribe_size.png", res = 300, units = "in", h = 5, w = 8)
+ggplot(aes(x = toothtype, y = mn, color = real_class, size = n), data = acc_ind) + geom_point()  + geom_line(aes(group = real_class), size = 0.5) + xlab("Tooth Type") + ylab("Accuracy") + scale_color_discrete(name = "True Tribe") + scale_size_continuous(name = "Sample Size")
+dev.off()
+
 
 ####################################
-#Other accuracy stats Tribe
+#Kappa Tribe
 ####################################
 load("./results/results_rf_tribe.rda")
 load("./results/results_svm_radial_tribe.rda")
@@ -187,6 +210,7 @@ for (proj in c("I", "OV", "I-PC", "OV-PC","EFA")) {print(proj)
   for (tooth in c("LM1", "LM2", "LM3", "UM1", "UM2", "UM3")) {
     #Random Forest
     mat <- results_rf_tribe[[proj]][[tooth]]
+    mat$pred_class <- factor(mat$pred_class, levels = levels(mat$real_class))
     xtab <- table(mat$pred_class, mat$real_class)
     cm <- caret::confusionMatrix(xtab)
     kappa <- cm$overall["Kappa"]
@@ -237,6 +261,7 @@ for (proj in c("I", "OV", "I-PC", "OV-PC","EFA")) {print(proj)
   for (tooth in c("LM1", "LM2", "LM3", "UM1", "UM2", "UM3")) {
     #Random Forest
     mat <- results_rf_species_given_tribe[[proj]][[tooth]]
+    mat$pred_class <- factor(mat$pred_class, levels = levels(mat$real_class))
     xtab <- table(mat$pred_class, mat$real_class)
     cm <- caret::confusionMatrix(xtab)
     kappa <- cm$overall["Kappa"]
@@ -245,6 +270,7 @@ for (proj in c("I", "OV", "I-PC", "OV-PC","EFA")) {print(proj)
     
     #SVM Linear
     mat <- results_svm_linear_species_given_tribe[[proj]][[tooth]]
+    mat$pred_class <- factor(mat$pred_class, levels = levels(mat$real_class))
     xtab <- table(mat$pred_class, mat$real_class)
     cm <- caret::confusionMatrix(xtab)
     kappa <- cm$overall["Kappa"]
@@ -253,6 +279,7 @@ for (proj in c("I", "OV", "I-PC", "OV-PC","EFA")) {print(proj)
     
     #SVM Radial
     mat <- results_svm_radial_species_given_tribe[[proj]][[tooth]]
+    mat$pred_class <- factor(mat$pred_class, levels = levels(mat$real_class))
     xtab <- table(mat$pred_class, mat$real_class)
     cm <- caret::confusionMatrix(xtab)
     kappa <- cm$overall["Kappa"]
@@ -262,8 +289,9 @@ for (proj in c("I", "OV", "I-PC", "OV-PC","EFA")) {print(proj)
   }
 }
 
+
 res$proj <- factor(res$proj, levels = c("EFA","I","OV","I-PC","OV-PC"))
-res <- res %>% filter(method != "XG")
+
 
 png("./figures/species_given_tribe_kappa_size.png", res = 300, units = "in", h = 5, w = 8)
 library(ggplot2)
@@ -318,8 +346,9 @@ res$proj <- factor(res$proj, levels = c("EFA","I","OV","I-PC","OV-PC"))
 res <- res %>% filter(method != "XG")
 names(res)[4:5] <- c("sens", "spec")
 
-
-
+library(xtable)
+out <- res[,c("tooth","tribe","proj","method","sens","spec")] %>% pivot_wider(names_from = c(proj,method), values_from = c(sens,spec))
+xtable(out)
 
 ggplot(aes(x = sens, y = spec, color = tribe, shape = proj), data = res %>% filter(method == "RF")) + geom_point(aes(group = method)) + facet_grid(toothnum ~ toothchar ) +  
   geom_line(aes(group = method)) + xlab("Feature Generation Method") + ylab("Kappa")
@@ -327,8 +356,7 @@ ggplot(aes(x = sens, y = spec, color = tribe, shape = proj), data = res %>% filt
   
 png("./figures/tribe_sens_and_spec_size.png", res = 300, units = "in", h = 5, w = 8)
 library(ggplot2)
-ggplot(aes(x = proj, y = accuracy, colour = method, group = method), data = res) + geom_point(aes(group = method)) + facet_grid(toothnum~toothchar) + 
-  geom_line(aes(group = method)) + xlab("Feature Generation Method") + ylab("Kappa")
+ggplot(aes(x = sens, y = spec, colour = proj, group = tribe), data = res %>% filter(method == "SVM-R")) + geom_point(aes(group = method)) + facet_grid(toothnum ~ toothchar ) + xlab("Specificity") + ylab("Sensitivity")
 dev.off()
 
 
@@ -349,3 +377,32 @@ dev.off()
 # ggplot(aes(x = toothtype, y = mn, color = real_class, size = n), data = acc_ind) + geom_point()  + geom_line(aes(group = real_class), size = 0.5) + xlab("Tooth Type") + ylab("Accuracy") + scale_color_discrete(name = "True Tribe") + scale_size_continuous(name = "Sample Size")
 # dev.off()
 
+####################################################################
+###Tribe ROC curves
+####################################################################
+library(ggplot2)
+library(pROC)
+load("./results/results_rf_tribe.rda")
+load("./results/results_svm_radial_tribe.rda")
+load("./results/results_svm_linear_tribe.rda")
+
+res <- data.frame()
+for (proj in c("I", "OV", "I-PC", "OV-PC","EFA")) {print(proj)
+  for (tooth in c("LM1", "LM2", "LM3", "UM1", "UM2", "UM3")) {
+    #Random Forest
+    mat <- results_svm_radial_tribe[[proj]][[tooth]]
+    for (tribe in c("Alcelaphini","Antilopini","Bovini","Hippotragini","Neotragini","Reduncini","Tragelaphini")){
+      
+      #define object to plot
+      rocobj <- roc(mat$real_class == tribe, mat$Alcelaphini)
+      res <- rbind(res,data.frame(tribe = tribe, method = "SVM-R", proj = proj, tooth = tooth,tooth_num = substring(tooth,3,3), tooth_char= substring(tooth,1,2), x = 1-rocobj$specificities, y = rocobj$sensitivities))
+      
+      
+    }
+    
+  }
+}
+
+png("./figures/tribe_roc_size.png", res = 300, units = "in", h = 5, w = 8)
+ggplot(aes(x = x, y = y, color = tribe), data = res %>% filter(proj == "I" & method == "SVM-R"))+ geom_abline(slope= 1, lty=2, alpha = 0.5) + geom_path() + facet_grid(tooth_num ~ tooth_char) + xlab("") + ylab("") + scale_x_continuous(breaks = c(0,0.5,1), labels = c(0,0.5,1)) + scale_y_continuous(breaks = c(0,0.5,1), labels = c(0,0.5,1)) 
+dev.off()
--- code/R/results_summary_gladysvale.R
+++ code/R/results_summary_gladysvale.R
@@ -4,21 +4,17 @@ for (toothtype in c("LM1","LM2","LM3","UM1","UM2","UM3")){
   res[[toothtype]] <- read.csv(paste0("./gladysvale_predictions/",toothtype,"_tribe_individual.csv"))
 }
 
-
 res_df <- do.call(rbind,res)
 res_df$tooth_loc <- substring(res_df$type,1,2)
 res_df$tooth_loc_num <- substring(res_df$type,3,3)
 library(xtable)
+#Table 3
 xtable(table(res_df$tooth_loc_num,res_df$tooth_loc))
 
-library(ggplot2)
-ggplot(aes(x = type, y = Alcelaphini), data = res_df) + geom_boxplot()
-
-
-table(res_df$type,res_df$pred_class)
-table(res_df$pred_class == "Alcelaphini")
-mean(res_df$pred_class == "Alcelaphini")
-
+##############################
+#Table 6 I
+##############################
+xtable(table(res_df$type,res_df$pred_class))
 
 #Overall results
 res <- list()
@@ -30,16 +26,10 @@ for (toothtype in c("LM1","LM2","LM3","UM1","UM2","UM3")){
 res_df <- do.call(rbind,res)
 res_df$tooth_loc <- substring(res_df$type,1,2)
 res_df$tooth_loc_num <- substring(res_df$type,3,3)
-library(xtable)
-xtable(table(res_df$tooth_loc_num,res_df$tooth_loc))
-
-library(ggplot2)
-ggplot(aes(x = type, y = Alcelaphini), data = res_df) + geom_boxplot()
-
-
-table(res_df$type,res_df$pred_class)
-table(res_df$pred_class == "Alcelaphini")
-mean(res_df$pred_class == "Alcelaphini")
+##############################
+#Table 6 O
+##############################
+xtable(table(res_df$type,res_df$pred_class))
 
 
 
@@ -69,8 +59,11 @@ res_df$tooth_loc_num <- substring(res_df$type,3,3)
 
 res_df <- merge(res_df, tribe_and_species, by.x = "pred_class", by.y = "species", all.x = TRUE)
 
+##############################
+#Table 7 I
+##############################
 xtable(table(res_df$type,paste0(res_df$tribe,res_df$pred_class)))
-table(res_df$type,paste0(res_df$tribe,res_df$pred_class))
+#table(res_df$type,paste0(res_df$tribe,res_df$pred_class))
 
 
 
@@ -90,15 +83,10 @@ res_df$tooth_loc_num <- substring(res_df$type,3,3)
 res_df <- merge(res_df, tribe_and_species, by.x = "pred_class", by.y = "species", all.x = TRUE)
 
 
-#xtable(table(res_df$type,res_df$pred_class))
+##############################
+#Table 7 O
+##############################
 xtable(table(res_df$type,paste0(res_df$tribe,res_df$pred_class)))
-table(res_df$type,paste0(res_df$tribe,res_df$pred_class))
 
 
 
-
-#buselaphus, taurinus, gnou, dorcas
-sum(res_df$pred_class == "gnou")
-sum(res_df$pred_class == "buselaphus")
-sum(res_df$pred_class == "dorcas")
-mean(res_df$pred_class == "taurinus")
